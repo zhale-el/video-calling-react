385b7822c808d01ccf2c65c04a5df4e35d94f8b4
zhale-el <zhalehelmi@gmail.com>
2025-09-30 13:41:46 +0330

    db setting and rnv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
@@ -7,4 +8,8 @@ app.get("/", (req, res) => {
   res.send("Hello welcom");
 });
 
-app.listen(ENV.PORT, () => console.log("Server started on port:", ENV.PORT));
+app.listen(ENV.PORT, () => {
+  console.log("Server started on port:", ENV.PORT);
+
+  connectDB();
+});
