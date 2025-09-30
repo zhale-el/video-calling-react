e7cb39026bf5e8cb3d3b1e58d26b923e8c91d8a9
zhale-el <zhalehelmi@gmail.com>
2025-09-30 21:15:45 +0330

    stream

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
-import { User } from "../models/user.model.js";
+import { User } from "../models/user.model.js"; // Import the User model
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
@@ -22,6 +23,14 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.image,
+    });
+
+    // await addUserToPublicChannels(newUser.clerkId.toString());
   }
 );
 
@@ -32,6 +41,8 @@ const deleteUserFromDB = inngest.createFunction(
     await connectDB();
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
   }
 );
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,31 @@
+import "../instrument.mjs";
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./config/inngest.js";
+import chatRoutes from "./routes/chat.route.js";
+
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
 app.use(express.json()); //req.body
 app.use(clerkMiddleware()); // req.auth will be available in the request object
 
-// Set up the "/api/inngest" (recommended) routes with the serve handler
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get("/debug-sentry", (req, res) => {
+  throw new Error("My first Sentry error!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello welcom");
 });
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+
+Sentry.setupExpressErrorHandler(app);
 
 const startServer = async () => {
   try {
